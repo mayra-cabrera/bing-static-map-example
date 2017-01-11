c71531aa639f7a019c54bb9acf5b648b7cda5ce3
Mayra Cabrera <mcabrera1087@gmail.com>
2017-01-11 13:02:11 -0600

    more stuff
    It does not work :(

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
   var pushpins = { pp: "38.889586530732335,-77.05010175704956;23;LM", pp: "38.88772364638439,-77.0472639799118;7;KM", pp: "38.890479451480054,-77.04744637012482;1;VM", pp: "38.8896854931628,-77.03519403934479;45;WM" };
-  var requestUrl = "http://dev.virtualearth.net/REST/v1/Imagery/Map/Road/?key=AlpLVmb_car8H3QsPBKpMkFvk3GrivnTsxvk1ba-jU4rXgkGu1I_DCJe2hbgccen";
+  var requestUrl = "http://dev.virtualearth.net/REST/v1/Imagery/Map/Road/?key=lolz";
   console.log(jQuery.param(pushpins));
 
   $.ajax({
     type: "POST",
     contentType: 'text/plain;charset=utf-8',
-    url: requestUrl + '&responseType=' + 'image/png',
+    url: encodeURIComponent(requestUrl) + '&responseType=' + 'image/png',
+    processData: false,
     data: jQuery.param(pushpins),
     success: function (response) {
-      console.log("Success")
-      $("#main_image").attr("src", response)
-     // $('#image').html(response);
+      console.log("Success");
+      var image = decodeURIComponent(escape(atob(response))); 
+      document.getElementById('content').innerHTML = '<img src="data:image/png;base64,' + image + '"/>';
+
     },
     error: function (xhr, ajaxOptions, thrownError) {
       console.log(xhr.status);
