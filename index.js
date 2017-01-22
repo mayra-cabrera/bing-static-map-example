4cb68363e0e5039e9a097a28ca8091d87935b6eb
Mayra Cabrera <mcabrera1087@gmail.com>
2017-01-21 21:15:35 -0600

    Removes useless console.log's

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   var pushpins = { pp: "38.889586530732335,-77.05010175704956;23;LM", pp: "38.88772364638439,-77.0472639799118;7;KM", pp: "38.890479451480054,-77.04744637012482;1;VM", pp: "38.8896854931628,-77.03519403934479;45;WM" };
   var requestUrl = "http://dev.virtualearth.net/REST/v1/Imagery/Map/Road/?key=lolz";
-  console.log(jQuery.param(pushpins));
 
   $.ajax({
     type: "POST",
@@ -10,7 +9,6 @@ $(document).ready(function() {
     processData: false,
     data: jQuery.param(pushpins),
     success: function (response) {
-      console.log("Success");
       var image = decodeURIComponent(escape(atob(response))); 
       document.getElementById('content').innerHTML = '<img src="data:image/png;base64,' + image + '"/>';
 
